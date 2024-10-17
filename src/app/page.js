5c33d15e9b5d0042f25255d566f6495a810f7a16
Ali <aliuajibade317@gmail.com>
2024-10-17 11:17:40 -0700

    add blue background

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { Categories, PostCard, PostWidget } from "../../components";
+
 
 const posts = [
   {title: "What is Islamic Monotheism", excerpt: " Ibn Abi'l Izz Al-Hanafi"},
@@ -10,16 +12,12 @@ export default function Home() {
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post, index) => (
-            <div>
-              {post.title}
-              {post.excerpt}
-            </div>
-          ))}
+         {posts.map((post) => <PostCard post={post} key={post.title} />)}
         </div>
-        <div className="lg:col-span-8 col-span-1">
+        <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
-
+            <PostWidget />
+            <Categories />
           </div>
         </div>
 
